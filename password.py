5593ebfc9167f0bd6d85ea511a8aa3a6cc4e0951
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-30 11:06:35 +0300

    Add a method to find if user exist

--- password.py
+++ password.py
@@ -41,6 +41,22 @@ class User:
     for user in cls.user_list:
       if user.phone_number == number:
         return user
+
+  @classmethod
+  def user_exist(cls,number):
+    '''
+    Method that checks if a user exists from the user list.
+    Args:
+        number: Phone number to search if it exists
+    Returns :
+        Boolean: True or false depending if the user exists
+    '''
+    for user in cls.user_list:
+      if user.phone_number == number:
+        return True
+
+    return False
+
 # while True:
 #   password = input("What is the password?")
 #   if password == "nick1111":
