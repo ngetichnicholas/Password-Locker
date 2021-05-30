f9880b781475602dbc9daada8d93e28d7ada0f7d
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-30 09:53:40 +0300

    Add a setup method with an object to run before each user test cases

--- password_test.py
+++ password_test.py
@@ -1,5 +1,6 @@
 import unittest
 import pyperclip
+from password import User
 
 
 class TestUser(unittest.TestCase):
@@ -9,3 +10,8 @@ class TestUser(unittest.TestCase):
   Args:
     unittest.TestCase: TestCase class that helps in creating user test cases
   '''
+def setUp(self):
+  '''
+  Set up method to run before each test cases in User
+  '''
+  self.new_user = User("Nicholas","Ngetich","NgetichNick","0725470732","ngetichnicholas903@gmail.com") #creating user object
