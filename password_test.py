4e29d56936b3bec5a884fd63298a485d5199ed89
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-30 09:02:38 +0300

    Add TestUser class that defines test cases for the user class behaviours

--- password_test.py
+++ password_test.py
@@ -1,2 +1,11 @@
 import unittest
 import pyperclip
+
+
+class TestUser(unittest.TestCase):
+  '''
+  Test class that defines test cases for the user class behaviours.
+
+  Args:
+    unittest.TestCase: TestCase class that helps in creating user test cases
+  '''
