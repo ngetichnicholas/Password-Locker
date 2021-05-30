fd31cbbe314c8641e200f7cc7f881932a9ff7849
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-30 10:23:20 +0300

    Add test case for initialization of user object save user object

--- password_test.py
+++ password_test.py
@@ -2,7 +2,6 @@ import unittest
 import pyperclip
 from password import User
 
-
 class TestUser(unittest.TestCase):
   '''
   Test class that defines test cases for the user class behaviours.
@@ -21,3 +20,21 @@ def tearDown(self):
   tearDown method that does clean up after each test case has run.
   '''
   User.user_list = []
+
+def test_init(self):
+  '''
+  test_init test case to test if the object is initialized properly
+  '''
+
+  self.assertEqual(self.new_user.first_name,"Nicholas")
+  self.assertEqual(self.new_user.last_name,"Ngetich")
+  self.assertEqual(self.new_user.user_name,"NgetichNick")
+  self.assertEqual(self.new_user.phone_number,"0725470732")
+  self.assertEqual(self.new_user.email,"ngetichnicholas903@gmail.com")
+def test_save_user(self):
+  '''
+  test_save_user test case to test if the user object is saved into
+  the user list
+  '''
+  self.new_user.save_user() #saving the user
+  self.assertEqual(len(User.user_list),1)
