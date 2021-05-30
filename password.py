71c643083b3fd988e1ea9e3f5ad320f57977b972
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-30 08:38:40 +0300

    Add user list

--- password.py
+++ password.py
@@ -3,6 +3,10 @@ class User:
   '''
   Class that creates new intances of users
   '''
+
+  user_list = [] # creates an empty user list
+
+  
 # while True:
 #   password = input("What is the password?")
 #   if password == "nick1111":
